32be643e6333bee226b6ec2c9867e51e12ef4bee
erik <erik.allen.martin@gmail.com>
2024-01-30 14:21:19 -0500

    Added checks for max weight and CoG. Updated README.

--- HW1.cpp
+++ HW1.cpp
@@ -68,6 +68,7 @@ int main() {
     fuel_moment = moment_func(fuel_moment_arm, usable_fuel_weight);
     baggage_moment = moment_func(baggage_moment_arm, baggage_weight);
 
+    // Main values for CG calculation.
     double moment_sum = front_moment + rear_moment + fuel_moment + baggage_moment + empt_weight_moment;
     double total_weight = empt_weight + front_seat_weight + rear_seat_weight + usable_fuel_weight + baggage_weight;
 
@@ -78,14 +79,86 @@ int main() {
     // std::cout << moment_sum << ", " << total_weight << std::endl; 
     // std::cout << center_of_gravity << std::endl;
 
-    if (center_of_gravity < 82.1) {
-
-    }
-    else if (center_of_gravity > 84.7) {
-
-    }
-
+    // Weight limit
     if (total_weight > 2950) {
+        int over_weight = total_weight - 2950;
+        double remove_fuel = over_weight + 0.01;
+        // If the requirements cannot be met, this notifies as such
+        if (remove_fuel > usable_fuel_weight) {
+            std::cout << "Cannot remove enough fuel to meet design limitations." << std::endl;
+        }
+        // Reevaluates the CoG and the total weight, as well as how much fuel needs to be removed.
+        else {
+            std::cout << "Must remove " << remove_fuel << " lbs of fuel to meet weight limit." << std::endl;
+            double new_usable_fuel = usable_fuel_weight - remove_fuel;
+            double new_moment = front_moment + rear_moment + baggage_moment + empt_weight_moment 
+                                    + moment_func(fuel_moment_arm, new_usable_fuel);
+            double new_total_weight = total_weight - usable_fuel_weight + new_usable_fuel;
+            double new_center_gravity = new_moment/new_total_weight;
+
+            center_of_gravity = new_center_gravity;
+            usable_fuel_weight = new_usable_fuel;
+            total_weight = new_total_weight;
+
+            std::cout << "New center of gravity location is: " << center_of_gravity << std::endl;
+        }
+
+        // CoG limit. 
+        // A more elegant solution is possible, will figure that out later.
+        if (center_of_gravity < 82.1) {
+            std::cout << "Center of gravity does not meet design limitations (lower than allowable)." << std::endl;
+            
+            double old_fuel_weight = usable_fuel_weight;
+            // Increments the fuel to meet the requirement. ONLY for under limit.
+            while (center_of_gravity < 82.1) {
+                double new_usable_fuel = usable_fuel_weight - 0.01;
+                double new_moment_sum = front_moment + rear_moment + baggage_moment + empt_weight_moment
+                                        + moment_func(fuel_moment_arm, new_usable_fuel);
+                double new_total_weight = total_weight - usable_fuel_weight + new_usable_fuel;
+                double new_center_gravity = new_moment_sum/new_total_weight;
+
+                usable_fuel_weight = new_usable_fuel, total_weight = new_total_weight; 
+                center_of_gravity = new_center_gravity;
+            }
+            double fuel_difference = old_fuel_weight - usable_fuel_weight; 
+
+            std::cout << "New usable fuel weight is: " << usable_fuel_weight << "lbs. Must remove "
+                << fuel_difference << "lbs of fuel." << std::endl;
+            std::cout << "New total weight is: " << total_weight << std::endl;
+            std::cout << "New center of gravity is: " << center_of_gravity << std::endl;
+        }
+
+        // Increments the fuel weight to meet limiations. ONLY for over limit.
+        else if (center_of_gravity > 84.7) {
+            std::cout << "Center of gravity does not meet design limitations (greater than allowable)." << std::endl;
+            // Maintains old fuel weight for later calculations.
+            double old_fuel_weight = usable_fuel_weight;
+
+            while (center_of_gravity > 84.7) { 
+                // Checks to determine whether it is possible to maintain weight limit.
+                if (total_weight > 2950) {
+                    std::cout << "Design limitations cannot be met." << std::endl;
+                }
+                else {
+                    // Assigns placeholder values to do calculations.
+                    double new_usable_fuel = usable_fuel_weight + 0.01;
+                    double new_moment_sum = front_moment + rear_moment + baggage_moment + empt_weight_moment
+                                            + moment_func(fuel_moment_arm, new_usable_fuel);
+                    double new_total_weight = total_weight - usable_fuel_weight + new_usable_fuel;
+                    double new_center_gravity = new_moment_sum/new_total_weight;
+
+                    usable_fuel_weight = new_usable_fuel, total_weight = new_total_weight; 
+                    center_of_gravity = new_center_gravity;
+                }
+            }
+            // Fuel difference.
+            double fuel_difference = usable_fuel_weight - old_fuel_weight;
+
+            std::cout << "New usable fuel weight is: " << usable_fuel_weight << "lbs. Must remove " 
+                << fuel_difference << "lbs of fuel." << std::endl;
+            std::cout << "New total weight is: " << total_weight << std::endl;
+            std::cout << "New center of gravity is: " << center_of_gravity << std::endl;
+        }
 
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Homework 1
+## Homework 1
+
+# Question 1
+Determines if plane is within flight limitations. If not, it attempts to create a situation such that this is possible. There is certainly a better way of determining how much fuel must be added/removed to reach limitations for the center of gravity, I will come back to that at a later date. 
+
+Solution currently works. Tested a fuel amount (231lbs) that would put the plane overweight, then the center of gravity would be less than the allowable.
